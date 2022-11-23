6135d696d424a62f1736a1e8eb97ade7b3977aa9
Okpainmo <okpainmoandrew@gmail.com>
2022-11-23 07:18:38 +0100

    init(tailwindcss): project styling to be done with tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [],
+
+  darkMode: 'class',
+  theme: {
+    // extend: {},
+    screens: {
+      xsm: '450px',
+      // => @media (min-width: 450px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '770px',
+      // => @media (min-width: 770px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px'
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  plugins: []
+};
