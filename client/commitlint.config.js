05e0c8cfc7e16ae95ce5e08637c009018119240a
Okpainmo <okpainmoandrew@gmail.com>
2022-11-23 07:15:42 +0100

    config(husky, commitlint, prettier, eslint): added the listed tools for proper workflow management

---
+++ client/commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'init',
+        'config',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'bulk',
+        'delete',
+        'assets'
+      ]
+    ]
+  }
+};
