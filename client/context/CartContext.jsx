56d27ef93ef3df7197d17b42a0d08e9f1b673668
Okpainmo <okpainmoandrew@gmail.com>
2022-12-02 04:24:52 +0100

    refactor(contextAPI, styles): updates on context API and styles

--- client/context/CartContext.jsx
+++ client/context/CartContext.jsx
@@ -1,11 +1,43 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import { doc, collection, addDoc, getDocs, updateDoc, setDoc, deleteDoc } from 'firebase/firestore';
+import { db } from '../firebaseConfig';
 
 export const CartContext = createContext();
 
 function CartContextProvider({ children }) {
-  const number = 12;
+  const [dbCart, setDbCart] = useState('');
 
-  return <CartContext.Provider value={number}>{children}</CartContext.Provider>;
+  async function getCart() {
+    await getDocs(collection(db, 'userCart')).then((data) => {
+      const dbCartData = data.docs.map((item) => {
+        return { ...item.data(), id: item.id };
+      });
+
+      setDbCart(dbCartData);
+
+      // console.log(
+      //   data.docs.map((item) => {
+      //     return { ...item.data(), id: item.id };
+      //   })
+      // );
+
+      // setDbCart(...item.data);
+      // console.log(dbCartData);
+    });
+  }
+
+  useEffect(() => {
+    getCart();
+    // setDbCart(getDocs);
+  }, []);
+
+  // console.log(dbCart);
+
+  const number = 5;
+
+  return (
+    <CartContext.Provider value={{ number, dbCart, setDbCart }}>{children}</CartContext.Provider>
+  );
 }
 
 export default CartContextProvider;
