9e8d659e3c570f1ecf6fc02759ed0b85b51db267
Okpainmo <okpainmoandrew@gmail.com>
2022-12-04 06:18:46 +0100

    progress: context API updates

--- client/context/CartContext.jsx
+++ client/context/CartContext.jsx
@@ -1,35 +1,37 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
 import { doc, collection, addDoc, getDocs, updateDoc, setDoc, deleteDoc } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
+import { ContractContext } from './ContractContext';
 
 export const CartContext = createContext();
 
 function CartContextProvider({ children }) {
-  const [dbCart, setDbCart] = useState('');
+  const [dbCart, setDbCart] = useState([]);
+  const { connectionStatus } = useContext(ContractContext);
 
   async function getCart() {
-    await getDocs(collection(db, 'userCart')).then((data) => {
-      const dbCartData = data.docs.map((item) => {
-        return { ...item.data(), id: item.id };
+    const userWalletAddress = await window.ethereum.request({ method: 'eth_accounts' });
+    const cartId = `userCart_${userWalletAddress[0]}`;
+
+    if (typeof window.ethereum !== 'undefined' && userWalletAddress.length !== 0) {
+      await getDocs(collection(db, cartId)).then((data) => {
+        const dbCartData = data.docs.map((item) => {
+          return { ...item.data(), id: item.id };
+        });
+        // console.log(dbCartData);
+        if (dbCartData !== undefined) {
+          setDbCart(dbCartData);
+        } else {
+          console.log('waiting for dbCartData');
+        }
       });
-
-      setDbCart(dbCartData);
-
-      // console.log(
-      //   data.docs.map((item) => {
-      //     return { ...item.data(), id: item.id };
-      //   })
-      // );
-
-      // setDbCart(...item.data);
-      // console.log(dbCartData);
-    });
+    }
   }
 
   useEffect(() => {
     getCart();
     // setDbCart(getDocs);
-  }, []);
+  }, [dbCart, connectionStatus]);
 
   // console.log(dbCart);
 
---
+++ client/context/ContractContext.jsx
@@ -0,0 +1,62 @@
+import React, { createContext, useState } from 'react';
+
+export const ContractContext = createContext();
+
+function ContractContextProvider({ children }) {
+  const [connectionStatus, setConnectionStatus] = useState(false);
+
+  async function connect() {
+    if (typeof window.ethereum !== 'undefined') {
+      try {
+        await ethereum.request({ method: 'eth_requestAccounts' });
+        window.ethereum.request({ method: 'eth_accounts' })[0];
+        setConnectionStatus(true);
+      } catch (error) {
+        console.log(error);
+      }
+      //   connectButton.innerHTML = 'Connected';
+      const address = await ethereum.request({ method: 'eth_accounts' });
+      console.log(address);
+    } else {
+      //   setConnectionStatus(false);
+      alert(
+        'The metamask browser extension helps users connect their metamask crypto wallets to websites. It seems the extension is not installed on your browser. Please install the extension on your browser to be able to connect your metamask wallet to deeco.'
+      );
+    }
+  }
+
+  // async function getUserWalletAddress() {
+  //   if (typeof window.ethereum !== 'undefined') {
+  //     const address = await ethereum.request({ method: 'eth_accounts' });
+
+  //     return address;
+  //   }
+  // }
+
+  async function checkMetamaskAccount() {
+    // e.preventDefault();
+    const userWalletAddress = await window.ethereum.request({ method: 'eth_accounts' });
+    console.log(userWalletAddress.length);
+
+    if (typeof window.ethereum !== 'undefined' && userWalletAddress.length !== 0) {
+      console.log(userWalletAddress);
+      setConnectionStatus(true);
+      // console.log(connectionStatus);
+    } else {
+      setConnectionStatus(false);
+      // console.log(connectionStatus);
+
+      alert('wallet not found: NOT CONNECTED');
+    }
+  }
+
+  return (
+    <ContractContext.Provider
+      value={{ connect, connectionStatus, setConnectionStatus, checkMetamaskAccount }}
+    >
+      {children}
+    </ContractContext.Provider>
+  );
+}
+
+export default ContractContextProvider;
