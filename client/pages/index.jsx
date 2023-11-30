8e488e24c172d26fdd7cbbd3c37f2c793a16bd53
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-12-01 00:49:55 +0100

    style: improve store creation sucess page responsiveness and replace <img> with next/Image

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -4,6 +4,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import { isValidEmail } from '../utils/validation';
+import MobileUser from '../assets/images/undraw_mobile_user.png';
+import GoogleIcon from '../assets/images/devicon_google.png';
+import Line from '../assets/images/line.png';
 
 const LoginPage = () => {
   const router = useRouter();
@@ -58,7 +61,7 @@ const LoginPage = () => {
   return (
     <main className="bg-gray-100 flex justify-evenly">
       <div className="w-[30%] lg:flex items-end justify-end hidden mb-2 ">
-        <Image src="images/undraw_mobile_user.png" alt="Mobile User" />
+        <Image src={MobileUser} alt="Mobile User" />
       </div>
       <div className="w-[90%] md:w-[80%] lg:w-[40%] border rounded-sm bg-white shadow-md p-4 my-8 md:p-8">
         <h1 className="font-bold text-3xl text-center mt-4">Log in to Deeco</h1>
@@ -110,15 +113,15 @@ const LoginPage = () => {
         <div className="flex flex-col items-center justify-center mt-6">
           <div className="flex items-center justify-between w-full md:w-[80%]">
             <div className="w-[31%]">
-              <Image src="images/Line.png" alt="line" className="w-[100%] text-center" />
+              <Image src={Line} alt="line" className="w-[100%] text-center" />
             </div>
             <p className="text-xs lg:text-sm text-center text-gray-400 w-[32%]">Or continue with</p>
             <div className="w-[31%]">
-              <Image src="images/Line.png" alt="line" className="w-[100%] text-center" />
+              <Image src={Line} alt="line" className="w-[100%] text-center" />
             </div>
           </div>
           <div className="my-6">
-            <Image src="images/devicon_google.png" alt="google_logo" />
+            <Image src={GoogleIcon} alt="google_logo" />
           </div>
         </div>
       </div>
--- client/pages/store-creation-success.jsx
+++ client/pages/store-creation-success.jsx
@@ -31,7 +31,7 @@ function StoreCreationSuccess() {
                   filterUnits="userSpaceOnUse"
                   color-interpolation-filters="sRGB"
                 >
-                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
                   <feColorMatrix
                     in="SourceAlpha"
                     type="matrix"
