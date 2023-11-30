fe1ef92ce45d6afa28f640bf2e1d6cedfe56f85b
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-12-01 00:25:35 +0100

    refactor: replace <img> tag with Next Image in Store creation sucess page

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import Image from 'next/image';
 
 import { isValidEmail } from '../utils/validation';
 
@@ -57,7 +58,7 @@ const LoginPage = () => {
   return (
     <main className="bg-gray-100 flex justify-evenly">
       <div className="w-[30%] lg:flex items-end justify-end hidden mb-2 ">
-        <img src="images/undraw_mobile_user.png" alt="Mobile User" />
+        <Image src="images/undraw_mobile_user.png" alt="Mobile User" />
       </div>
       <div className="w-[90%] md:w-[80%] lg:w-[40%] border rounded-sm bg-white shadow-md p-4 my-8 md:p-8">
         <h1 className="font-bold text-3xl text-center mt-4">Log in to Deeco</h1>
@@ -109,15 +110,15 @@ const LoginPage = () => {
         <div className="flex flex-col items-center justify-center mt-6">
           <div className="flex items-center justify-between w-full md:w-[80%]">
             <div className="w-[31%]">
-              <img src="images/Line.png" alt="line" className="w-[100%] text-center" />
+              <Image src="images/Line.png" alt="line" className="w-[100%] text-center" />
             </div>
             <p className="text-xs lg:text-sm text-center text-gray-400 w-[32%]">Or continue with</p>
             <div className="w-[31%]">
-              <img src="images/Line.png" alt="line" className="w-[100%] text-center" />
+              <Image src="images/Line.png" alt="line" className="w-[100%] text-center" />
             </div>
           </div>
           <div className="my-6">
-            <img src="images/devicon_google.png" alt="google_logo" />
+            <Image src="images/devicon_google.png" alt="google_logo" />
           </div>
         </div>
       </div>
